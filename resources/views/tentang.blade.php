024c3b19d1611bb8d29b23ed479cb81e241a05c4
Luthfi Taufikurrahman <luthfi.indo04@gmail.com>
2025-01-05 23:47:32 +0700

    halaman_utama_done

--- resources/views/tentang.blade.php
+++ resources/views/tentang.blade.php
@@ -4,10 +4,10 @@
 
     <!-- Konten Halaman Tentang -->
     <div class="container mt-4">
+        <!-- Mengimpor bagian 'daftar_team' -->
+        @include('fitur.daftar_team')
         <!-- Mengimpor bagian 'contact_us' -->
         @include('fitur.contact_us')
 
-        <!-- Mengimpor bagian 'daftar_team' -->
-        @include('fitur.daftar_team')
     </div>
 @endsection
